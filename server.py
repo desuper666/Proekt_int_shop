72d7c597bfc7dd74175e6307b218208b8534d57c
desuper666 <Watdesuper@gmail.com>
2025-05-11 16:49:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -825,5 +825,5 @@ for filename, content in templates.items():
 os.makedirs('static/images', exist_ok=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
